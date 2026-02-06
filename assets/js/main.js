dd7c3c58a14bc595a63ab454191807fe95a5ac98
Saichul Huda <saichulhuda1995@gmail.com>
2026-02-06 22:23:00 +0700

    Tambah fitur swiper di portfolio & sesuaikan gambar portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,22 +212,6 @@
   });
 
   /**
-   * Initiate portfolio lightbox
-   */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfolio-lightbox",
-  });
-
-  /**
-   * Initiate portfolio details lightbox
-   */
-  const portfolioDetailsLightbox = GLightbox({
-    selector: ".portfolio-details-lightbox",
-    width: "90%",
-    height: "90vh",
-  });
-
-  /**
    * Portfolio details slider
    */
   new Swiper(".portfolio-details-slider", {
@@ -242,23 +226,19 @@
       type: "bullets",
       clickable: true,
     },
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
     },
   });
 
